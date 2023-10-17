a11c100f90dd758bcd913ed5bf7afe39974cc8f3
Abhiram0402 <Abhiramma2022@gmail.com>
2023-10-17 15:06:28 +0530

    Express.js

--- middleware/logEvents.js
+++ middleware/logEvents.js
@@ -10,19 +10,23 @@ const logEvents = async(message,logName) =>{
 const logItem = `${dateTime}\t ${uuid()} \t ${message} \n`;
 console.log(logItem);
 try{
-    if(!fs.existsSync(path.join(__dirname,'logs'))){
-        await fsPromises.mkdir(path.join(__dirname,'logs'));
+    if(!fs.existsSync(path.join(__dirname,'..','logs'))){
+        await fsPromises.mkdir(path.join(__dirname,'..','logs'));
     }
-    await fsPromises.appendFile(path.join(__dirname,'logs',logName),logItem);
+    await fsPromises.appendFile(path.join(__dirname,'..','logs',logName),logItem);
 
 } catch(err){
     console.log(err);
 }
 
 }
+const logger=(req,res,next) =>{
+    logEvents(`${req.method}\t${req.headers.origin}\t${req.url}`,'reqLog.txt');
+next();
+}
 
 
-module.exports = logEvents;
+module.exports = { logger,logEvents};
 
 // console.log(format(new Date(), 'yyyyMMdd\t HH:mm:ss'))
 
